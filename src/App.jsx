d8d1a5345f53005f486cfa4bb676c23a2696a1f0
ArmestoSantiago <contactosantiagoarmesto@gmail.com>
2024-02-06 22:14:28 -0300

    Change map to Leaftlet-react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Map } from './Map/Map';
+
+export function App () {
+  return (
+    <>
+      <p>SOS Patas</p>
+      <Map />
+    </>
+  );
+}
---
+++ src/Map/Map.jsx
@@ -0,0 +1,16 @@
+import { MapContainer } from 'react-leaflet/MapContainer';
+import { TileLayer } from 'react-leaflet/TileLayer';
+import { useMap } from 'react-leaflet/hooks';
+
+import './map.css';
+
+export function Map () {
+  return (
+    <MapContainer id='map' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+    </MapContainer>
+  );
+}
