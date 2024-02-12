cd23726a2cbf2544d40f8ab4331673f71d112434
ArmestoSantiago <contactosantiagoarmesto@gmail.com>
2024-02-12 16:43:11 -0300

    Refactoring map component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,18 @@
-
-import {
-  APIProvider,
-  Map,
-  AdvancedMarker,
-  Pin,
-  InfoWindow
-} from '@vis.gl/react-google-maps';
-
 import { useGetLocation } from './hooks/useGetLocation/useGetLocation';
-import { useMemo } from 'react';
+import { FeaturesButtons } from './components/FeaturesButton/FeaturesButtons.component';
+import { Map } from './components/Map/Map.component';
+import { OptionsMenu } from './components/OptionsMenu/OptionsMenu.component';
+import { SearchInput } from './components/SearchInput/SearchInput.component';
 
 export function App () {
   const { location, error } = useGetLocation();
-  const options = useMemo(() => ({
-    disableDefaultUI: true,
-    clickableIcons: false
-  }), []);
 
   return (
-    <>
-      {!error &&
-        <APIProvider apiKey='AIzaSyBiBjfqdenY-yYtegC9pkMkqs777R8Ubb8'>
-          <div style={{ height: '100vh' }}>
-            <Map zoom={13} center={location} options={options} />
-          </div>
-        </APIProvider>}
-    </>
+    <main>
+      <SearchInput />
+      <FeaturesButtons />
+      <OptionsMenu />
+      <Map coordenates={location} error={error} />
+    </main>
   );
 }
