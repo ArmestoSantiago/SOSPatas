68e30d35a9758af1ea7b7e7f63ff61bebff67f07
ArmestoSantiago <contactosantiagoarmesto@gmail.com>
2024-03-06 17:28:05 -0300

    New components for filters buttons

---
+++ src/components/FiltersButton/FiltersButton.jsx
@@ -0,0 +1,10 @@
+import './FiltersButton.css';
+
+export function FiltersButton () {
+  return (
+    <div className='container-filters-buttons'>
+      <button className='buttons-style'>Mascotas Perdidas</button>
+      <button className='buttons-style'>Animales Extraviados</button>
+    </div>
+  );
+}
