89232c86023a63eb3c9b072a93f1ff37c2c13b2a
ArmestoSantiago <contactosantiagoarmesto@gmail.com>
2024-03-11 11:24:32 -0300

    Modifed map's style cursor so is idenfied dynamically

--- src/components/Map/Map.option.js
+++ src/components/Map/Map.option.js
@@ -1,7 +1,10 @@
-export const MAP_OPTIONS = {
-  disableDefaultUI: true,
-  fullscreenOption: false,
-  maxZoom: 17,
-  minZoom: 10,
-  zoom: 14
+export const MAP_OPTIONS = (cursor = false) => {
+  return {
+    disableDefaultUI: true,
+    fullscreenOption: false,
+    maxZoom: 17,
+    minZoom: 10,
+    zoom: 14,
+    draggableCursor: cursor ? 'crosshair' : ''
+  };
 };
