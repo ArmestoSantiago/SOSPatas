8249a0d7261ee5dd475afc270cb502ebbde889b3
ArmestoSantiago <contactosantiagoarmesto@gmail.com>
2024-03-11 11:22:49 -0300

    Create modal to add animales to map

---
+++ src/components/AddLocationModal/AddLocationModal.component.jsx
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+import { AddLocationContext } from '../../context/AddLocation';
+
+export function AddLocationModal () {
+  const { openModal } = useContext(AddLocationContext);
+
+  
+}
