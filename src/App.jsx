4fd6c146a641f701dded5636c68fe94893713960
ArmestoSantiago <contactosantiagoarmesto@gmail.com>
2024-03-11 11:26:35 -0300

    Use new context to interact with add animal location modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useGetLocation } from './hooks/useGetLocation/useGetLocation';
 import { Map } from './components/Map/Map.component';
 import { NavigationOptions } from './components/NavigationOptions/Navigation.components';
 import { SearchCityProvider } from './context/SearchCity';
+import { AddLocationProvider } from './context/AddLocation';
+import { AddLocationModal } from './components/AddLocationModal/AddLocationModal.component';
 
 export function App () {
   const { location, error } = useGetLocation();
@@ -9,8 +11,11 @@ export function App () {
   return (
     <main>
       <SearchCityProvider>
-        <NavigationOptions />
-        <Map coords={location} error={error} />
+        <AddLocationProvider>
+          <AddLocationModal />
+          <Map coords={location} error={error} />
+          <NavigationOptions />
+        </AddLocationProvider>
       </SearchCityProvider>
     </main>
   );
