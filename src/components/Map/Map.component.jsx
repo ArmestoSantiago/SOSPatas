7702d8b13497476e83940410b44f40a5472ef654
ArmestoSantiago <contactosantiagoarmesto@gmail.com>
2024-03-12 17:51:24 -0300

    update map component

--- src/components/Map/Map.component.jsx
+++ src/components/Map/Map.component.jsx
@@ -1,22 +1,45 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { MAP_OPTIONS } from './Map.option';
 import { SearchCityContext } from '../../context/SearchCity';
+import { AddLocationContext } from '../../context/CrossHairContext';
 
 const API_KEY = import.meta.env.VITE_GOOGLEMAP_APIKEY;
 
-export function Map ({ coords, error }) {
+export function Map ({ coords, error, setOpenModal }) {
   if (error) return;
+  const { crosshairCursor, setCrosshairCursor } = useContext(AddLocationContext);
   const { cityCoords } = useContext(SearchCityContext);
-
   const centerCoords = cityCoords !== false ? cityCoords : coords;
+
+  useEffect(() => {
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        setCrosshairCursor(false);
+      }
+    });
+  }, []);
+
+  const handleClick = (e) => {
+    setOpenModal({
+      open: true,
+      coords: {
+        lat: e.lat,
+        lng: e.lng
+      }
+    });
+
+    setCrosshairCursor(false);
+  };
+  console.log(centerCoords.lng);
 
   return (
     <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
+        onClick={handleClick}
         bootstrapURLKeys={{ key: API_KEY }}
         center={centerCoords}
-        options={MAP_OPTIONS}
+        options={MAP_OPTIONS(crosshairCursor)}
         zoom={14}
       />
     </div>
