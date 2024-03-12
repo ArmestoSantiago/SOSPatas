567423016f3d6782827b6925fb13338a20e6f060
ArmestoSantiago <contactosantiagoarmesto@gmail.com>
2024-03-12 17:49:41 -0300

    add form and form style to create a new animal marker with ir position

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { useGetLocation } from './hooks/useGetLocation/useGetLocation';
 import { Map } from './components/Map/Map.component';
 import { NavigationOptions } from './components/NavigationOptions/Navigation.components';
 import { SearchCityProvider } from './context/SearchCity';
-import { AddLocationProvider } from './context/AddLocation';
+import { AddLocationProvider } from './context/CrossHairContext';
 import { AddLocationModal } from './components/AddLocationModal/AddLocationModal.component';
+import { useState } from 'react';
 
 export function App () {
   const { location, error } = useGetLocation();
+  const [openModal, setOpenModal] = useState(false);
 
   return (
     <main>
       <SearchCityProvider>
         <AddLocationProvider>
-          <AddLocationModal />
-          <Map coords={location} error={error} />
+          <Map coords={location} error={error} setOpenModal={setOpenModal} />
           <NavigationOptions />
+          {openModal && <AddLocationModal
+            openModal={openModal}
+            setOpenModal={setOpenModal}
+                        />}
         </AddLocationProvider>
       </SearchCityProvider>
     </main>
--- src/components/AddLocationModal/AddLocationModal.component.jsx
+++ src/components/AddLocationModal/AddLocationModal.component.jsx
@@ -1,8 +1,63 @@
-import { useContext } from 'react';
-import { AddLocationContext } from '../../context/AddLocation';
+import { useId } from 'react';
+import './AddLocationModal.css';
 
-export function AddLocationModal () {
-  const { openModal } = useContext(AddLocationContext);
+export function AddLocationModal ({ openModal, setOpenModal }) {
+  const animalTypeId = useId();
+  const animalStateId = useId();
+  const animalConditionId = useId();
+  const descriptionId = useId();
+  const imageId = useId();
 
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setOpenModal(false);
+
+    const fields = Object.fromEntries(new window.FormData(e.target));
+
+    console.log(fields);
+  };
+
+  return (
+    <div className='container-animal--form'>
+      <p className='text'>Ayudemos a que cada animal tenga un hogar</p>
+      <form onSubmit={handleSubmit}>
+        <div className='form-row'>
+          <div className='input-data'>
+            <label className='label-text' htmlFor={animalTypeId}>Tipo</label>
+            <select id={animalTypeId} name='animalType' className='select-options'>
+              <option value='dog'>Perro</option>
+              <option value='cat'>Gato</option>
+            </select>
+          </div>
+          <div className='input-data'>
+            <label className='label-text' htmlFor={animalConditionId}>Estado</label>
+            <select name='condition' id={animalConditionId} className='select-options'>
+              <option value='healty'>Sano</option>
+              <option value='hurt'>Herido</option>
+              <option value='badly'>Urgente</option>
+            </select>
+          </div>
+        </div>
+        <div className='input-data'>
+          <label className='label-text' htmlFor={animalStateId}>Estado</label>
+          <select name='state' id={animalStateId} className='select-options'>
+            <option value='street'>Callejero</option>
+            <option value='lost'>Extraviado</option>
+          </select>
+        </div>
+        <div className='img-file-box input-data'>
+          <label htmlFor={imageId}>Sube una foto:</label>
+          <input type='file' id={imageId} name='animalImage' accept='image/*' />
+        </div>
+        <div className='description-box'>
+          <label htmlFor={descriptionId}>Describe algo del animal</label>
+          <textarea id={descriptionId} className='description-text' type='text' name='descripcion' placeholder='color, comportamiento, raza, situación...' />
+        </div>
+        <div />
+        <div type='submit' className='submit-btn '>
+          <button type='submit' className='btn-grad'>AGREGAR</button>
+        </div>
+      </form>
+    </div>
+  );
 }
